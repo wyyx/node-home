8ec3a97efafc05494e4e1609827eb36d20a98a7f
wyyx <gnehcyx@163.com>
2018-09-19 11:12:08 +0800

    add home server

---
+++ server.js
@@ -0,0 +1,16 @@
+const http = require('http')
+const fs = require('fs')
+
+const port = process.env.PORT || 8080
+
+http
+	.createServer((req, res) => {
+		const homePage = fs.readFileSync('home.html', 'utf-8')
+		res.writeHead(200, {
+			'Content-Type': 'text/html'
+		})
+		res.end(homePage)
+	})
+	.listen(port, () => {
+		console.log('server listening port', port)
+	})
